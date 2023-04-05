05c3065a1b9de874b9fb3647a62b271dfa6fd65b
Jhonny Miñan <53890778+JhonnyMinian92@users.noreply.github.com>
2023-04-05 17:06:43 -0500

    Initial commit

---
+++ src/main/java/com/ecuapp/market/EcuappMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ecuapp.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcuappMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcuappMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecuapp/market/EcuappMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecuapp.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcuappMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
